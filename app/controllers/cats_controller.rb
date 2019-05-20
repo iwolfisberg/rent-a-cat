10b57af907ea29297d91a4f7c68b089f6cebb47e
nicolas varone <varonenico@gmail.com>
2019-05-20 17:17:57 +0200

    WIP dont merge

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,10 @@
 class CatsController < ApplicationController
   before_action :set_cat, only: [:show]
 
+  def index
+    @cats = Cat.all
+  end
+
   def show
     @user = current_user
     @cat.user_id = @user
