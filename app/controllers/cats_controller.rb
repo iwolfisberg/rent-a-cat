c35e5eaa744ec64d54de7e8a6d60b174916819d4
Iris <iris.wolfisberg@hotmail.fr>
2019-05-20 14:01:35 +0200

    Created method show model Cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,12 @@
 class CatsController < ApplicationController
-  def show
+  before_action :set_cat, only: [:show]
 
+  def show
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(params[:id])
   end
 end
