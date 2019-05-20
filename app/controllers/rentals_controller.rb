d5e8dd20b11ec0b8e04eeac44e5f8df6a5ecf7e8
Iris <iris.wolfisberg@hotmail.fr>
2019-05-20 16:03:53 +0200

    Done view without layout and partial form_rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,21 @@
 class RentalsController < ApplicationController
+  def create
+    @rental = Rental.new(params_rental)
+    @user = current_user
+    @rental.user = @user
+    @cat = Cat.find(params[:cat_id])
+    @rental.cat = @cat
+
+    if @rental.save
+      redirect_to cat_path(@cat) # A changer pour "my_profile"
+    else
+      render cat_path(@cat)
+    end
+  end
+
+  private
+
+  def params_rental
+    params.require(:rental).permit(:date_start, :date_end)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "cats#index"
-   resources :cats, only: [:index, :show, :new, :create]
-   resources :rentals, only: [:new, :create]
+   resources :cats, only: [:index, :show, :new, :create] do
+    resources :rentals, only: [:create]
+  end
 end
