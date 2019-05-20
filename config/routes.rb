a799ae79ebd1bd384a8ed389aea6f7b2eb8d2fc5
Iris <iris.wolfisberg@hotmail.fr>
2019-05-20 13:50:40 +0200

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "cats#index"
+   resources :cats, only: [:index, :show, :new, :create]
+   resources :rentals, only: [:new, :create]
 end
