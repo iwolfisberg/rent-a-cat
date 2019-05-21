d2332caa067a4ad7863a3e4f99d0bb094ee3b92d
Justine Tincq <jtincq@gmail.com>
2019-05-21 15:10:54 +0200

    display current user rentals on myprofile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def myprofile
     @user = current_user
-    @cats = Cat.where(user_id: @user.id)
+    @cats = Cat.where(user_id: @user.id).order(created_at: :desc)
+    @rentals = Rental.where(user_id: @user.id).order(created_at: :desc)
+    @cats_rented = []
+    @rentals.each do |rental|
+      cat = Cat.find(rental.cat_id)
+      @cats_rented.push(cat)
+    end
   end
 end
