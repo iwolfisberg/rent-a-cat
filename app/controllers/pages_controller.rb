ea8880e4f21acd01b0d2c9bbb9aee559b8654959
Justine Tincq <jtincq@gmail.com>
2019-05-21 15:33:38 +0200

    remarques corrigées

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,7 @@
 class PagesController < ApplicationController
   def myprofile
     @user = current_user
-    @cats = Cat.where(user_id: @user.id).order(created_at: :desc)
-    @rentals = Rental.where(user_id: @user.id).order(created_at: :desc)
-    @cats_rented = []
-    @rentals.each do |rental|
-      cat = Cat.find(rental.cat_id)
-      @cats_rented.push(cat)
-    end
+    @cats = Cat.where(user_id: @user).order(created_at: :desc)
+    @rentals = Rental.where(user_id: @user).order(created_at: :desc)
   end
 end
