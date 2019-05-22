f2155c3717b837dd3e8e01ded8152a2a611cb9d5
Iris <iris.wolfisberg@hotmail.fr>
2019-05-22 10:44:51 +0200

    Ajouter migration colonne user_photo dans users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_113514) do
+ActiveRecord::Schema.define(version: 2019_05_22_084130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_113514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "user_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
