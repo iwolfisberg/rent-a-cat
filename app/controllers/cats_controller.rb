8b1688f51bdf2cf9b65c7015ee91c46ff5d369ed
Justine Tincq <jtincq@gmail.com>
2019-05-23 15:38:39 +0200

    added map google in index

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,7 +3,16 @@ class CatsController < ApplicationController
   before_action :set_cat, only: [:show]
 
   def index
-    @cats = Cat.all.order(created_at: :desc)
+    @cats = Cat.where.not(latitude: nil, longitude: nil).order(created_at: :desc)
+    @markers = @cats.map do |cat|
+      {
+        lat: cat.latitude,
+        lng: cat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/flats/map_box")
+      }
+    end
   end
 
   def new
