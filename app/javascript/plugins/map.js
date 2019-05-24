f508990a03b6a059d3672e185d79e68ac0e0b1ea
Justine Tincq <jtincq@gmail.com>
2019-05-24 12:56:11 +0200

    set maxzoom

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -2,7 +2,7 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0, maxZoom: 14 });
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
@@ -13,4 +13,6 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
-}
+};
+
+
